b437dd5e619faee9ccaa0191cb83a39cf12b0daf
niranth <niranthcr@gmail.com>
2020-02-21 10:25:01 +0530

    Completed Module

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,13 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	var Max float32
+	var Items []Item
+}
 
 // Item stores item information
+type Item struct {
+	var Description string
+	var Price float32
+}
