3599ef0afa2ba557dccc5743e251ebbc4e8ca7b2
niranth <niranthcr@gmail.com>
2020-02-21 10:59:40 +0530

    Completed module

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -57,7 +57,7 @@ func (b *Budget) AddItem(description string, price float32) error {
 		return errors.New("errDoesNotFitBudget")
 	}
 	newItem := Item{Description: description, Price: price}
-	append(b.Items, newItem)
+	b.Items = append(b.Items, newItem)
 	return nil
 }
 
